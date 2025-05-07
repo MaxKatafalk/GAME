3252860fb4389086ccbcd76430855dc9b294e7bc
MaxKatafalk <max20060602@mail.ru>
2025-05-07 11:07:54 +1000

    о

--- main.py
+++ main.py
@@ -1,50 +1,13 @@
-import pygame
-import sys
+import pygame as pg
 
-# Инициализация Pygame
-pygame.init()
-hui='chlen'
-# Размер окна
-WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Мой первый Pygame")
-
-# Цвета
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-
-# Параметры шарика
-x, y = WIDTH // 2, HEIGHT // 2
-radius = 30
-speed_x, speed_y = 5, 3
-
-# Основной цикл
-clock = pygame.time.Clock()
+pg.init()
+screen = pg.display.set_mode(1000, 1000)
 
 running = True
+
 while running:
-    clock.tick(60)  # Ограничение до 60 кадров в секунду
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Логика движения шарика
-    x += speed_x
-    y += speed_y
-
-    # Отскоки от стенок
-    if x - radius <= 0 or x + radius >= WIDTH:
-        speed_x = -speed_x
-    if y - radius <= 0 or y + radius >= HEIGHT:
-        speed_y = -speed_y
-
-    # Рисование
-    screen.fill(WHITE)  # Заливка фона белым
-    pygame.draw.circle(screen, RED, (x, y), radius)  # Рисуем шарик
-
-    pygame.display.flip()  # Обновляем экран
-
-# Завершение работы
-pygame.quit()
-sys.exit()
+	for ev in pg.event.get():
+		if ev.type == pg.QUIT:
+			running = False
+			
+pg.quit()
