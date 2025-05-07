7949027036f6abf82414a845041019167f88a246
MaxKatafalk <max20060602@mail.ru>
2025-05-07 12:46:16 +1000

    j

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import math
 
 pg.init()
 screen = pg.display.set_mode((1000, 1000))
@@ -16,10 +17,43 @@ class Tank:
 		self.speedRotation = 2
 		self.surface = pg.Surface((width, height), pg.SRCALPHA)
 
+	def update(self, moving):
+		if not moving:
+			self.angle = (self.angle + self.speedRotation) % 360
+		else:
+			radian = math.radians(self.angle)
+			self.x += math.cos(radian) * 2
+			self.y += math.sin(radian) * 2
+
+	def draw(self, surface):
+		rectSurf = pg.Surface((self.width, self.height), pg.SRCALPHA)
+		pg.draw.rect(rectSurf, self.color, (0, 0, self.width, self.height))
+		rotated = pg.transform.rotate(rectSurf, -self.angle)
+		rect = rotated.get_rect(center=(self.x, self.y))
+		surface.blit(rotated, rect)
+
+
+tanks = [Tank(300, 300, 100, 40, (0, 255, 0)), Tank(500, 500, 100, 40, (0, 0, 255))]
+
+buttons = [pg.K_UP, pg.K_w]
+
 running = True
+
 while running:
 	for ev in pg.event.get():
 		if ev.type == pg.QUIT:
 			running = False
+
+	screen.fill((30, 30, 30))
+
+	keys = pg.key.get_pressed()
+
+	for index, tank in enumerate(tanks):
+		moving = keys[buttons[index]]
+		tank.update(moving)
+		tank.draw(screen)
+
+	pg.display.flip()
+	clock.tick(60)
 			
 pg.quit()
