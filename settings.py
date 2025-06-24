fccd02b6249ff18f40fae82ed4e33c73ec4cd915
MaxKatafalk <max20060602@mail.ru>
2025-06-25 08:11:26 +1000

    туц

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import pygame as pg
 
 pg.init()
 
-SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1800, 1000
-TILE_SIZE = 200
+SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1620, 900
+TILE_SIZE = 180
 FONT = pg.font.Font(None, 60)
 
 screen = pg.display.set_mode(SCREEN_SIZE)
